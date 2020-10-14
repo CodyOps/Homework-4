54ba6b8d06941a58e5f9eb84e4e5aae67d903dd7
Cody Chen <codychen1@gmail.com>
2020-10-14 14:16:27 -0600

    added pseudocode to js file

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 //Code Quiz
+var correctAnswer;
+var incorrectAnswer;
+var timer = 200;
+
+//Function to display new quesitons after a set amount of time
+//Function to have eventlistener when a correct answer is chosen
+//Function to have eventListener when inccorect answer is chosen and reduce total time
+//Function to save intials and score
