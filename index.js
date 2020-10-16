dffed9cda32254ba760c9495fcd468a18b02e622
Cody Chen <codychen1@gmail.com>
2020-10-16 11:15:08 -0600

    added more variables linking to html

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 //Code Quiz
 
 //VARIABLE DECLARATION
-var correctAnswer;
-var incorrectAnswer;
-var timer = 200;
+var timer = document.querySelector("#navbarNav");
+var question = document.querySelector("#quizQuestion");
+var answerOne = document.querySelector("#answer-1");
+var answerTwo = document.querySelector("#answer-2");
+var answerThree = document.querySelector("#answwer-3");
+var answerFour = document.querySelector("#answer-4");
+
+
 
 var questions = [
   "QuestionOne",
@@ -16,5 +21,23 @@ var questions = [
 //List of questions and variable to hold all the questions
 //logic for deciding on the right answer
 //eventhandler when a user clicks the button
+//
 
 //FUNCTIONS
+
+function startQuestion() {
+  var currentQuesiton = questions[currentQuestionsIndex];
+  questions.textcontent = currentQuesiton.title
+  choices.innterHTML = ""
+}
+
+choices.foreach(functions (choice, i)) {
+  var choiceBtn = document.createElement("button");
+  choiceBtn.setAttribute("class", "choice");
+  choiceBtn.setAttribute()
+  choiceBtn.textcontent = i + 1 "." choices
+}
+
+//create a function that will run on click what the use clicked with event object - 
+//what the current question is and is what is click equal to the correct answer
+//If yes what next and if wrong then got next 
